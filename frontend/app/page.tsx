b83eaed3468f743818128efa7f20c24003ff425d
pikacoder44 <bc220401268smh@vu.edu.pk>
2026-02-24 00:23:43 +0500

    Updated heading

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 export default function Home() {
-  const [file, setFile] = useState(null);           // newly selected file
+  const [file, setFile] = useState(null); // newly selected file
   const [submittedFile, setSubmittedFile] = useState(null); // file actually submitted
   const [result, setResult] = useState(null);
   const [confidence, setConfidence] = useState(null);
@@ -35,7 +35,10 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-center text-black bg-zinc-50 font-sans">
-      <h3 className="text-3xl font-semibold mb-4">Please Upload the picture</h3>
+      <h3 className="text-3xl font-semibold  pb-0 mb-0">
+        Please Upload the picture
+      </h3>
+      <h5 className=" text-gray-500">or capture it using your camera</h5>
 
       <form
         className="flex flex-col items-center gap-4 bg-gray-100 p-6 rounded-lg shadow-md"
@@ -51,6 +54,7 @@ export default function Home() {
           onChange={(e) => setFile(e.target.files[0])}
           className="border border-gray-300 bg-gray-500 text-white rounded-lg p-2 cursor-pointer"
         />
+      
         <button
           type="submit"
           className="bg-green-600 px-4 py-2 rounded-2xl hover:bg-green-800 hover:text-white cursor-pointer"
@@ -70,7 +74,9 @@ export default function Home() {
           />
           <h4 className="text-xl font-semibold mb-2">Prediction Result:</h4>
           <p className="text-lg">Predicted Class: {result}</p>
-          <p className="text-lg">Confidence: {(confidence * 100).toFixed(2)}%</p>
+          <p className="text-lg">
+            Confidence: {(confidence * 100).toFixed(2)}%
+          </p>
         </div>
       )}
     </div>
