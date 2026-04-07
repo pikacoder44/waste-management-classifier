43220601061fcb353697955e9a72c36fc69c67b1
pikacoder44 <pikaop125@gmail.com>
2026-04-07 16:42:14 +0500

    Added collections names for mongodb

---
+++ backend/app/database/collections.py
@@ -0,0 +1,6 @@
+from .connection import db
+
+waste_collection = db["waste_images"]
+user_collection = db["users"]
+recommendation_collection = db["recommendations"]
+model_collection = db["models"]
