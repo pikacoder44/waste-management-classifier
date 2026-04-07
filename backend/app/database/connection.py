f126c427d732c05bcfba5ecc01e932bca3ec3185
pikacoder44 <pikaop125@gmail.com>
2026-04-07 16:42:41 +0500

    Basic connection to mongoDB

---
+++ backend/app/database/connection.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = MongoClient(MONGO_URL)
+db = client["waste_classifier"]
+waste_collection = db["waste_collection"]
