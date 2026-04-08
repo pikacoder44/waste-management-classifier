e2254fb0ebedfc4d332556dc3f556a3b5a2781b2
pikacoder44 <pikaop125@gmail.com>
2026-04-08 16:02:47 +0500

    Added Waste schema

---
+++ backend/app/models/waste.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel,  Field
+from typing import Optional
+
+
+class Waste(BaseModel):
+    imageId: Optional[str] = Field(None, alias="_id")
+    userId: str  # foreign key to User model
+    filePath: str
+    uploadDate: str
+
+    class Config:
+        allow_population_by_field_name = (
+            True  # Allow using field names instead of aliases when creating instances
+        )
+        arbitrary_types_allowed = (
+            True  # Allow arbitrary types (like ObjectId) without validation errors
+        )
