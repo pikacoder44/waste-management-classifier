8288e4aa5d15803bcc2f5a891c770c446a3820a9
pikacoder44 <pikaop125@gmail.com>
2026-04-08 16:00:59 +0500

    Added Admin Schema

---
+++ backend/app/models/admin.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class Admin(BaseModel):
+    adminId: Optional[str] = Field(None, alias="_id")
+    hashed_password: str
+    role: str = "admin"  # Default role is 'admin'
+
+    class Config:
+        allow_population_by_field_name = True # Allow using field names instead of aliases when creating instances
+        arbitrary_types_allowed = True # Allow arbitrary types (like ObjectId) without validation errors
