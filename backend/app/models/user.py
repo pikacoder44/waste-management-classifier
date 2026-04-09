cf69dd13d28afe412a436c6ec2766dfc7b8160a2
pikacoder44 <pikaop125@gmail.com>
2026-04-09 16:43:13 +0500

    Redefined User Schema - userId will be generated automatically, introduced a username field

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel,  Field
-from typing import Optional
+from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
-    userId: Optional[str] = Field(None, alias="userId")  # MongoDB's ObjectId will be stored as a string
-    password: str
-    role: str = "user"  # Default role is 'user', can be 'admin' for administrators
+    username: str = Field(..., min_length=3, max_length=20)
+    password: str = Field(..., min_length=8, max_length=30)
+    role: str = "user"
 
     class Config:
-        populate_by_name = True  # Allow using field names instead of aliases when creating instances
-        arbitrary_types_allowed = True # Allow arbitrary types (like ObjectId) without validation errors
+        populate_by_name = (
+            True  # Allow using field names instead of aliases when creating instances
+        )
+        arbitrary_types_allowed = (
+            True  # Allow arbitrary types (like ObjectId) without validation errors
+        )
