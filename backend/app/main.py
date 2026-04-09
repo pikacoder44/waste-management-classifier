46c44387c9fd5cf7821989b2fb92822fb7be3d2e
pikacoder44 <pikaop125@gmail.com>
2026-04-09 16:41:36 +0500

    Included auth routes and removed unnecessary predict route(Will implement it later)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database.connection import db
-from app.api.routes import predict
 from app.api.routes import user_routes
+from app.api.routes import auth_routes
 
 app = FastAPI()
 
@@ -11,5 +11,5 @@ def test_db():
     return {"collections": db.list_collection_names()}
 
 
-app.include_router(predict.router)
 app.include_router(user_routes.router)
+app.include_router(auth_routes.router)
