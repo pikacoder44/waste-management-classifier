86f4dc6b2e1d1e5808bb969640fe5a8a95544425
pikacoder44 <pikaop125@gmail.com>
2026-04-09 16:42:21 +0500

    Redefined Admin Schema as per need

--- backend/app/models/admin.py
+++ backend/app/models/admin.py
@@ -1,12 +1,15 @@
 from pydantic import BaseModel, Field
-from typing import Optional
 
 
 class Admin(BaseModel):
-    adminId: Optional[str] = Field(None, alias="adminId")
-    password: str
-    role: str = "admin"  # Default role is 'admin'
+    adminId: str = Field(..., min_length=1)
+    password: str = Field(..., min_length=1)
+    role: str = "admin"  # Default role is 'admin', can be 'user' for regular users
 
     class Config:
-        populate_by_name = True # Allow using field names instead of aliases when creating instances
-        arbitrary_types_allowed = True # Allow arbitrary types (like ObjectId) without validation errors
+        populate_by_name = (
+            True  # Allow using field names instead of aliases when creating instances
+        )
+        arbitrary_types_allowed = (
+            True  # Allow arbitrary types (like ObjectId) without validation errors
+        )
