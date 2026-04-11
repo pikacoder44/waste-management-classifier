945d13d369d5497bceefbee11d31d603d048872a
pikacoder44 <pikaop125@gmail.com>
2026-04-11 17:02:23 +0500

    Conigure CLoudinary (Api key - secret key etc)

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,16 @@
 import os
+import cloudinary
+from dotenv import load_dotenv
 
 # JWT Configuration
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# CLoudinary Configuration
+load_dotenv()  # Load environment variables from .env file
+cloudinary.config(
+    cloud_name=os.getenv("CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+)
