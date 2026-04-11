ff1cd0c22182a5d7a7a8b57f3e33127bd031bf77
pikacoder44 <pikaop125@gmail.com>
2026-04-11 17:03:25 +0500

    Updated this model to contain all waste image info including result

--- backend/app/models/waste.py
+++ backend/app/models/waste.py
@@ -1,11 +1,23 @@
-from pydantic import BaseModel,  Field
+from pydantic import BaseModel, Field
 from typing import Optional
 
 
 class Waste(BaseModel):
     userId: str  # foreign key to User model
-    filePath: str
+    filePath: Optional[str] = Field(
+        None, description="The file path where the uploaded image is stored"
+    )
     uploadDate: str
+    predictedLabel: Optional[str] = Field(
+        None, description="The predicted class label for the waste item"
+    )
+    confidence: Optional[float] = Field(
+        None, description="The confidence score of the prediction (0 to 1)"
+    )
+    inferenceTime: Optional[float] = Field(
+        None,
+        description="The time taken for the AI model to make the prediction (in seconds)",
+    )
 
     class Config:
         populate_by_name = (
