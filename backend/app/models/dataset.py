a49de7793876e646034e2e29ff51b1b8060c6f8c
pikacoder44 <pikaop125@gmail.com>
2026-04-16 15:13:08 +0500

    Remove unused imports from dataset model

--- backend/app/models/dataset.py
+++ backend/app/models/dataset.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
-from typing import Optional, List, Literal
-from bson import ObjectId
+from typing import Optional
 
 
 class Dataset(BaseModel):
