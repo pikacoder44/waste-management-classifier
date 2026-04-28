41b7caf248043a81d66534c404b9f537fbe1edc3
pikacoder44 <pikaop125@gmail.com>
2026-04-28 21:58:50 +0500

    Remove Cloudinary configuration from config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,4 @@
 import os
-import cloudinary
 from dotenv import load_dotenv
 
 # JWT Configuration
@@ -7,10 +6,4 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-# CLoudinary Configuration
 load_dotenv()  # Load environment variables from .env file
-cloudinary.config(
-    cloud_name=os.getenv("CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-)
