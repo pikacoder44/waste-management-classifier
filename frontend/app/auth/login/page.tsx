700ce7449b08606da507bc95843069902ef0c9a8
pikacoder44 <pikaop125@gmail.com>
2026-05-02 17:13:03 +0500

    Remove React import to streamline code in Login component

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../../context/AuthContext";
 import {
