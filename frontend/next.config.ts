58f8f7cfe3cbd7a68b2a79e6829dd6c3e23f7fbf
pikacoder44 <pikaop125@gmail.com>
2026-05-18 16:18:45 +0500

    Add additional remote patterns for image loading in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: process.env.NODE_ENV === "development",
+    dangerouslyAllowLocalIP: true,
     remotePatterns: [
       {
         protocol: "http",
@@ -12,7 +13,31 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "::1",
+        port: "8000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
         hostname: "localhost",
+        port: "8000",
+        pathname: "/dataset/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/dataset/**",
+      },
+      {
+        protocol: "http",
+        hostname: "::1",
         port: "8000",
         pathname: "/dataset/**",
       },
