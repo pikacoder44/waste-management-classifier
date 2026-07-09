7500932be7490b534d3d1b93490c2401b20d05bd
pikacoder44 <pikaop125@gmail.com>
2026-07-09 15:54:21 +0500

    Removed useless Config class

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -6,7 +6,3 @@ class User(BaseModel):
     username: str = Field(..., min_length=3, max_length=20)
     password: str = Field(..., min_length=8, max_length=30)
     role: Literal["user", "admin"] = "user"
-
-    class Config:
-        populate_by_name = True
-        arbitrary_types_allowed = True
