b3f610c0cb6ffc31f76769d0cfa934ced1af96ed
pikacoder44 <pikaop125@gmail.com>
2026-07-17 00:05:09 +0500

    Fixed formatDate Function pattern

--- frontend/app/utils/formatDate.ts
+++ frontend/app/utils/formatDate.ts
@@ -1,9 +1,11 @@
 export default function formatDate(dateString: string): string {
   try {
-    return new Date(dateString).toLocaleDateString("en-GB", {
+    return new Date(dateString).toLocaleDateString("en-US", {
       day: "numeric",
       month: "long",
       year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
     });
   } catch {
     return dateString;
