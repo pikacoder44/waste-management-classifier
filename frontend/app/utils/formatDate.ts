291b09cfa6a3c94c818d8027456e56162ac6c226
pikacoder44 <pikaop125@gmail.com>
2026-07-17 00:16:34 +0500

    Fixed formatDate func

--- frontend/app/utils/formatDate.ts
+++ frontend/app/utils/formatDate.ts
@@ -1,9 +1,8 @@
 export default function formatDate(dateString: string): string {
   try {
     return new Date(dateString).toLocaleDateString("en-US", {
+      month: "short",
       day: "numeric",
-      month: "long",
-      year: "numeric",
       hour: "2-digit",
       minute: "2-digit",
     });
