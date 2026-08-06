240fe1bba357ddf91242c7006b969dd75efb4bbc
pikacoder44 <pikaop125@gmail.com>
2026-08-06 15:23:29 +0500

    Added cloudinary config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,5 @@
 import os
+import cloudinary
 from dotenv import load_dotenv
 
 # Load environment variables
@@ -9,3 +10,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 # Access token lifetime in minutes.
 ACCESS_TOKEN_EXPIRE_MINUTES = 480  # 8 hours
+
+
+# Cloudinary Configuration
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
